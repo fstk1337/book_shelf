01e9fedeff0b6ee5ac395c3d6c4e102eba6b4469
fstk1337 <vit.shvaichuk@gmail.com>
2021-01-10 20:40:53 +0300

    initial commit

---
+++ src/java/com/example/app/services/BookRepository.java
@@ -0,0 +1,38 @@
+package com.example.app.services;
+
+import com.example.web.dto.Book;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class BookRepository implements ProjectRepository<Book> {
+
+    private final Logger logger = Logger.getLogger(BookRepository.class);
+    private final List<Book> repo = new ArrayList<>();
+
+    @Override
+    public List<Book> retrieveAll() {
+        return new ArrayList<>(repo);
+    }
+
+    @Override
+    public void store(Book book) {
+        book.setId(book.hashCode());
+        logger.info("store new book: " + book);
+        repo.add(book);
+    }
+
+    @Override
+    public boolean removeItemById(Integer bookIdToRemove) {
+        for(Book book: retrieveAll()) {
+            if (book.getId().equals(bookIdToRemove)) {
+                logger.info("remove book completed: " + book);
+                return repo.remove(book);
+            }
+        }
+        return false;
+    }
+}
