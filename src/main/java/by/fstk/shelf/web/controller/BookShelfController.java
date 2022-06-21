c43208d75d5f8b26680c6517a0828a491754842f
fstk1337 <vit.shvaichuk@gmail.com>
2022-06-21 22:27:16 +0300

    removing book functionality added

--- src/main/java/by/fstk/shelf/web/controller/BookShelfController.java
+++ src/main/java/by/fstk/shelf/web/controller/BookShelfController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/books")
@@ -37,5 +38,12 @@ public class BookShelfController {
         return "redirect:/books";
     }
 
-
+    @PostMapping("/remove")
+    public String removeBook(@RequestParam("bookId") Integer bookId) {
+        if (bookService.removeBookById(bookId)) {
+            return "redirect:/books";
+        } else {
+            return "book_shelf";
+        }
+    }
 }
